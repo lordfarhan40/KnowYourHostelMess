3fac25d8d395c18e6d80c8a7f512e284b2937078
Mohammed Farhan <lordfarhan40@gmail.com>
2018-03-01 17:04:29 +0530

    Created initial session handling and login

---
+++ admin.js
@@ -0,0 +1,107 @@
+var passport = require('passport');
+var Strategy = require('passport-local').Strategy;
+const sha = require('sha256');
+const users = require('./models/users.js');
+
+// Configure the local strategy for use by Passport.
+//
+// The local strategy require a `verify` function which receives the credentials
+// (`username` and `password`) submitted by the user.  The function must verify
+// that the password is correct and then invoke `cb` with a user object, which
+// will be set at `req.user` in route handlers after authentication.
+passport.use(new Strategy({
+    usernameField:'uid',
+    passwordField:'password'
+  },
+  function(uid, password, cb) {
+    password=sha(password);
+    users.getUserById(uid, function(err, user) {
+      if (err) { return cb(err); }
+      if (!user) { return cb(null, false); }
+      if (user.password != password) { return cb(null, false); }
+      return cb(null, user);
+    });
+  }));
+
+
+// Configure Passport authenticated session persistence.
+//
+// In order to restore authentication state across HTTP requests, Passport needs
+// to serialize users into and deserialize users out of the session.  The
+// typical implementation of this is as simple as supplying the user ID when
+// serializing, and querying the user record by ID from the database when
+// deserializing.
+passport.serializeUser(function(user, cb) {
+  cb(null, user.uid);
+});
+
+passport.deserializeUser(function(uid, cb) {
+  users.getUserById(uid,(err,user)=>
+  {
+    if(err||user===undefined){
+      return cb("There was some error");
+    }
+    cb(undefined,user);
+  });
+});
+
+function setUpRoutes(app){
+  
+  //Adding middleware to parse read post attributes and session
+  app.use(require('body-parser').urlencoded({ extended: true }));
+  app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+
+  // Initialize Passport and restore authentication state, if any, from the
+  // session.
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.post('/create_user',
+  require('connect-ensure-login').ensureLoggedIn('/login'),
+  function(req, res){
+    if(user.is_admin==0){
+      res.send("You are not the admin, you cannot create another user");
+      return;
+    }
+
+    var userDetails={
+      uid:request.body.uid,
+      password:sha(request.body.password),
+      is_admin:request.body.is_admin,
+      contact:request.body.contact,
+      hostel_id:request.body.hostel_id,
+      name:request.body.name,
+    }
+    users.createUser(userDetails,(err,res)=>
+    {
+      if(!err&&res==true)
+      {
+        res.send("The user got inserted!");
+      }else
+      {
+        res.send("The user insertion failed!");
+      }
+    });
+  });
+
+  app.get('/login',
+  function(req, res){
+    res.render('LoginPage.hbs');
+  });
+
+  //takes in uid and password as post paramenters to login
+  app.post('/login', 
+  passport.authenticate('local',{ failureRedirect: '/login' }),
+  function(req, res) {
+    res.send("Login successfull");
+  });
+
+  app.get('/logout',
+  function(req, res){
+    req.logout();
+    res.redirect('/');
+  });
+}
+
+module.exports={
+    setUpRoutes
+}
